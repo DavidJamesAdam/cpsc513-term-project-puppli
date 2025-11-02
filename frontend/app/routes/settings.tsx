26456367d4a99fd37cb8f10082d9fd5a0fc9123d
David Adam <david.adam1@ucalgary.ca>
2025-11-02 10:34:22 -0700

    feature: added nav links to nav dropdown and added header to all current pages

--- frontend/app/routes/settings.tsx
+++ frontend/app/routes/settings.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/settings";
+import Header from "../components/header/header";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,9 +10,11 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Settings() {
   return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>Settings page</h1>
-      <p>Here is a paragraph</p>
-    </main>
+    <>
+      <Header />
+      <main style={{ backgroundColor: "var{--bg-color}" }}>
+        <p>Settings page</p>
+      </main>
+    </>
   );
 }
