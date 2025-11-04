7c8f0da9e6dbee043688c7338c9d6dbcda25215e
arhamxrahman <rahmanarham3@gmail.com>
2025-11-03 20:18:38 -0700

    Firebase_admin venv check

--- backend/firebase_service.py
+++ backend/firebase_service.py
@@ -1,5 +1,5 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
+import firebase_admin # type: ignore
+from firebase_admin import credentials, firestore # type: ignore
 import os
 
 def initialize_firebase():
