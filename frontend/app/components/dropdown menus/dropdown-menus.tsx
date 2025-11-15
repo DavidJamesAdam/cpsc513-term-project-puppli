42007706daf23d95a8cbe7f7d203306a749e466e
30207333 <viann.hu@ucalgary.ca>
2025-11-14 22:02:36 -0700

    Added settings to drop down menu

--- frontend/app/components/dropdown menus/dropdown-menus.tsx
+++ frontend/app/components/dropdown menus/dropdown-menus.tsx
@@ -83,6 +83,14 @@ export function MainNavMenu() {
             Messages
           </Link>
         </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <div className="menu-icon">
+            <img src="assets\icons\Settings icon.svg" />
+          </div>
+          <Link className="menu-text" to="/settings">
+            Settings
+          </Link>
+        </MenuItem>
       </Menu>
     </div>
   );
