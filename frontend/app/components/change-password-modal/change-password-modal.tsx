9bd1d51abfd88c29b66bdc720f1c7afa79677c88
David Adam <david.adam1@ucalgary.ca>
2025-11-16 20:21:24 -0700

    feature: nearly finished password reset modal

--- frontend/app/components/change-password-modal/change-password-modal.tsx
+++ frontend/app/components/change-password-modal/change-password-modal.tsx
@@ -1,14 +1,31 @@
 import * as React from "react";
+import Button from "@mui/material/Button";
 
 export default function ChangePasswordModal() {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
   return (
     <div className="change-password-modal">
+      <button className="close-btn">
+        <img src="assets\icons\Close icon.svg" />
+      </button>
       <form>
-        <label htmlFor="fname">First name:</label>
-        <input type="text" id="fname" name="fname" />
-        <label htmlFor="lname">Last name:</label>
-        <input type="text" id="lname" name="lname" />
-        <input type="button"></input>
+        <div className="inputGroup">
+          <label htmlFor="currentPass">Please enter current password:</label>
+          <input type="text" className="input" name="currentPass" />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="newPass">Please enter new password:</label>
+          <input type="text" className="input" name="newPass" />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="reenterPass">Please re-enter new password:</label>
+          <input type="text" className="input" name="reenterPass" />
+        </div>
+        <div className="inputGroup buttonRow">
+          <Button variant="contained" id="submit">
+            Submit
+          </Button>
+        </div>
       </form>
     </div>
   );
