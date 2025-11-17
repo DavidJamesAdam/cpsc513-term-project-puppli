564672146b1a50a0becfa7230e1c93a33e34542d
David Adam <david.adam1@ucalgary.ca>
2025-11-17 11:09:39 -0700

    feature: updating styles

--- frontend/app/components/change-password-modal/change-password-modal.tsx
+++ frontend/app/components/change-password-modal/change-password-modal.tsx
@@ -4,8 +4,22 @@ import Button from "@mui/material/Button";
 export default function ChangePasswordModal() {
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
   return (
-    <div className="change-password-modal">
-      <button className="close-btn">
+    <div
+      className="change-password-modal"
+      style={{
+        borderRadius: "40px",
+        border: "1px solid rgba(255, 132, 164, 1)",
+        width: "50%",
+        height: "50%",
+        boxShadow: "5px 10px 10px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        backgroundColor: "rgba(224, 205, 178, 1)",
+      }}
+    >
+      <button className="close-btn" style={{margin: '10px'}}>
         <img src="assets\icons\Close icon.svg" />
       </button>
       <form>
