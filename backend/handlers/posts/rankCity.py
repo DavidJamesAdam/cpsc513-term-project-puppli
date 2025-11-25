f72ab8dd4b9583772a7de7ccd7e66c62753c6845
T-Smithy <tanis.smith@ucalgary.ca>
2025-11-25 15:09:50 -0700

    update rank_city to use Location object

--- backend/handlers/posts/rankCity.py
+++ backend/handlers/posts/rankCity.py
@@ -1,21 +1,29 @@
 from firebase_service import db
 from fastapi import FastAPI, HTTPException
+from classes.location import Location
 
-async def rank_city(user_city: str):
+#fix class use if location stored as JSON instead of string
+async def rank_city(user_location: str):
         
     try:
+        #convert user's location to Location object
+        user_location = Location.from_string(user_location)
+
         # Get all documents from 'posts' collection
         docs = db.collection('posts').stream()
 
         # Convert documents to dictionary format
         results = []
         for doc in docs:
+            #get post's (other user's) location and convert to Location object
             doc_data = doc.to_dict()
             post_user = doc_data['UserId']
             post_user = db.collection('users').document(post_user).get().to_dict()
-            post_location = post_user['location'].split(',')[0]
+            post_location = post_user['location']
+            post_location = Location.from_string(post_location)
+        
             #match 'location' to user's 'location' 
-            if str(post_location) in str(user_city):
+            if post_location.city == user_location.city:
                 doc_data['id'] = doc.id  # Include document ID
                 results.append(doc_data)
         
@@ -25,3 +33,8 @@ async def rank_city(user_city: str):
         return results
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error fetching posts: {str(e)}")
+    
+
+
+
+
