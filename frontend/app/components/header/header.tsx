1a5b53e117ab3c9106f6d2b99de24457cb7da115
Arham Rahman <80564106+arhamxrahman@users.noreply.github.com>
2025-12-03 10:53:08 -0700

    header optimized for mobile

--- frontend/app/components/header/header.tsx
+++ frontend/app/components/header/header.tsx
@@ -2,15 +2,18 @@ import {
   MainNavMenu,
 } from "../dropdown menus/nav-menu";
 import NotificationMenu from "../dropdown menus/notification-menu";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function Header() {
+  const matches = useMediaQuery("(min-width: 600px)");
+
   return (
     <div
       style={{
         width: "100%",
         height: "10%",
-        paddingLeft: 30,
-        paddingRight: 30,
+        paddingLeft: matches ? 30 : 10,
+        paddingRight: matches ? 30 : 10,
         paddingTop: 15,
         paddingBottom: 15,
         backgroundColor: "#FFC2CF",
@@ -18,20 +21,28 @@ export default function Header() {
         justifyContent: "space-between",
         alignItems: "center",
         display: "inline-flex",
+        boxSizing: "border-box",
       }}
     >
       {/* Logo */}
-      <div>
+      <div style={{ flex: "0 0 auto" }}>
         <MainNavMenu />
       </div>
 
       {/* App name */}
-      <div>
-        <h1 style={{ fontSize: "54px", color: "var(--font-color)" }}>Puppli</h1>
+      <div style={{ flex: "1 1 auto", textAlign: "center", overflow: "hidden" }}>
+        <h1 style={{
+          fontSize: matches ? "54px" : "clamp(24px, 8vw, 40px)",
+          color: "var(--font-color)",
+          margin: 0,
+          whiteSpace: "nowrap",
+        }}>
+          Puppli
+        </h1>
       </div>
 
       {/* Notification Bell */}
-      <div>
+      <div style={{ flex: "0 0 auto" }}>
         <NotificationMenu />
       </div>
     </div>
